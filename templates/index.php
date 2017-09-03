5ff9b2249801bf3bd2537997cc68060b2a5569f5
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2017-09-03 12:40:06 +0300

    Просто черновик

--- templates/index.php
+++ templates/index.php
@@ -32,14 +32,14 @@
         </select>
     </div>
     <ul class="lots__list">
-        <? foreach ($lots_list as $lot) :?>
+        <? foreach ($lots_list as $key => $lot) :?>
             <li class="lots__item lot">
                 <div class="lot__image">
                     <img src="<?= $lot["src"] ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?= $lot["category"] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?= htmlspecialchars($lot["title"]) ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="lot.php<?= '?id='.$key ?>"><?= htmlspecialchars($lot["title"]) ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
