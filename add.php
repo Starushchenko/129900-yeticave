e0722b476f3d68ae981eb74963c10f9a3f35645b
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2017-09-07 10:38:00 +0300

    lot form logic added

---
+++ add.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once('functions.php');
+require_once('lotsdata.php');
+
+$is_auth = (bool)rand(0, 1);
+
+$user_name = 'Константин';
+$user_avatar = 'img/user.jpg';
+
+// ставки пользователей, которыми надо заполнить таблицу
+$bets = [
+    ['name' => 'Иван', 'price' => 11500, 'ts' => strtotime('-' . rand(1, 50) . ' minute')],
+    ['name' => 'Константин', 'price' => 11000, 'ts' => strtotime('-' . rand(1, 18) . ' hour')],
+    ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) . ' hour')],
+    ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
+];
+
+// Массив категорий
+$lots_categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
+// Валидация формы добавления лота
+$number_inputs = ['lot-rate', 'lot-step'];
+$errors = [];
+$page_content = renderTemplate('add-lot', [
+    'errors' => $errors
+]);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    foreach ($_POST as $key => $value) {
+        if ($value == '') {
+            $errors[] = $key;
+        }
+        
+        if (in_array($key, $number_inputs) && !is_numeric($value)) {
+            $errors[] = $key;
+        }
+    }
+    if (isset($_FILES['photo'])) {
+        $photo_name = $_FILES['photo']['name'];
+        $photo_url = 'img/' . $photo_name;
+        move_uploaded_file($_FILES['photo']['tmp_name'], $photo_url);
+    }
+    
+    if (!count($errors)) {
+        $page_content = renderTemplate('lot-detail', [
+            'lot_title' => $_POST['lot-name'],
+            'lot_image' => $photo_url,
+            'lot_category' => $_POST['category'],
+            'lot_price' => $_POST['lot-rate'],
+            'lot_desc' => $_POST['message'],
+            'bets' => $bets
+        ]);
+    }
+}
+
+// Компиляция шаблона сайта
+echo renderTemplate('layout', [
+    'page_title' => 'Добавление лота',
+    'is_auth' => $is_auth,
+    'user_avatar' => $user_avatar,
+    'user_name' => $user_name,
+    'page_content' => $page_content,
+    'errors' => $errors
+]);
+?>
---
+++ templates/lot-detail.php
@@ -0,0 +1,47 @@
+<section class="lot-item container">
+    <h2><?= $lot_title ?></h2>
+    <div class="lot-item__content">
+        <div class="lot-item__left">
+            <div class="lot-item__image">
+                <img src="<?= $lot_image ?>" width="730" height="548" alt="Сноуборд">
+            </div>
+            <p class="lot-item__category">Категория: <span><?= $lot_category ?></span></p>
+            <p class="lot-item__description"><?= $lot_desc ?></p>
+        </div>
+        <div class="lot-item__right">
+            <div class="lot-item__state">
+                <div class="lot-item__timer timer">
+                    10:54:12
+                </div>
+                <div class="lot-item__cost-state">
+                    <div class="lot-item__rate">
+                        <span class="lot-item__amount">Текущая цена</span>
+                        <span class="lot-item__cost"><?= $lot_price ?></span>
+                    </div>
+                    <div class="lot-item__min-cost">
+                        Мин. ставка <span>12 000 р</span>
+                    </div>
+                </div>
+                <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post">
+                    <p class="lot-item__form-item">
+                        <label for="cost">Ваша ставка</label>
+                        <input id="cost" type="number" name="cost" placeholder="12 000">
+                    </p>
+                    <button type="submit" class="button">Сделать ставку</button>
+                </form>
+            </div>
+            <div class="history">
+                <h3>История ставок (<span>4</span>)</h3>
+                <table class="history__list">
+                    <? foreach ($bets as $key => $bet) : ?>
+                        <tr class="history__item">
+                            <td class="history__name"><?= $bet["name"] ?></td>
+                            <td class="history__price"><?= $bet["price"] ?>р</td>
+                            <td class="history__time"><?= calc_time_ago($bet["ts"]) ?></td>
+                        </tr>
+                    <? endforeach; ?>
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
