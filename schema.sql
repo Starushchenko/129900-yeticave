1bc480356c216b01a0a4b4c4d191ab321f9c635d
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2017-09-20 13:50:14 +0300

    fix schema

--- schema.sql
+++ schema.sql
@@ -3,23 +3,23 @@ USE yeticave;
 
 CREATE TABLE categories (
   id   INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  name CHAR(50)                                NOT NULL,
+  name VARCHAR(50)                             NOT NULL,
   UNIQUE INDEX (name)
 );
 
 CREATE TABLE lots (
   id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
   title       VARCHAR(100)                            NOT NULL,
-  description VARCHAR(1000)                           NOT NULL,
+  description TEXT                                    NOT NULL,
   create_date DATETIME                                NOT NULL,
   finish_date DATETIME                                NOT NULL,
-  image       VARCHAR(100)                            NOT NULL,
+  image       VARCHAR(100),
   start_price INT(10) UNSIGNED                        NOT NULL,
-  bet_step    SMALLINT(10) UNSIGNED                   NOT NULL,
+  bet_step    INT UNSIGNED                            NOT NULL,
   fav_count   SMALLINT(10) UNSIGNED                   NOT NULL,
-  author_id   SMALLINT(10) UNSIGNED                   NOT NULL,
-  winner_id   SMALLINT(10) UNSIGNED                   NOT NULL,
-  category_id SMALLINT(10) UNSIGNED                   NOT NULL,
+  author_id   INT UNSIGNED                            NOT NULL,
+  winner_id   INT UNSIGNED                            NOT NULL,
+  category_id INT UNSIGNED                            NOT NULL,
   INDEX (title),
   INDEX (start_price)
 );
@@ -28,8 +28,8 @@ CREATE TABLE bets (
   id        INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
   bet_date  DATETIME                                NOT NULL,
   bet_value SMALLINT(10) UNSIGNED                   NOT NULL,
-  author_id SMALLINT(10) UNSIGNED                   NOT NULL,
-  lot_id    SMALLINT(10) UNSIGNED                   NOT NULL,
+  author_id INT UNSIGNED                            NOT NULL,
+  lot_id    INT UNSIGNED                            NOT NULL,
   INDEX (bet_value)
 );
 
@@ -41,8 +41,8 @@ CREATE TABLE users (
   password_hash   VARCHAR(100)                            NOT NULL,
   avatar_path     VARCHAR(100),
   contacts        VARCHAR(500),
-  created_lots_id SMALLINT(10) UNSIGNED                   NOT NULL,
-  created_bets_id SMALLINT(10) UNSIGNED                   NOT NULL,
+  created_lots_id INT UNSIGNED                            NOT NULL,
+  created_bets_id INT UNSIGNED                            NOT NULL,
   INDEX (name),
   UNIQUE INDEX (email)
 );
