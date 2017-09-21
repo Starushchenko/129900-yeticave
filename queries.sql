249fc326eec526a794e7965c1b0ffac53b42af46
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2017-09-21 11:40:06 +0300

    refactor INSERT INTO .. (..) VALUES

--- queries.sql
+++ queries.sql
@@ -8,137 +8,128 @@ INSERT INTO categories (name) VALUES
   ('Инструменты'),
   ('Разное');
 
-INSERT INTO users
-SET
-  reg_date      = '2017-09-20',
-  email         = 'ignat.v@gmail.com',
-  name          = 'Игнат',
-  password_hash = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa',
-  avatar_path   = 'img/avatar.jpg',
-  contacts      = '+79787777712';
+INSERT INTO users (reg_date, email, name, password_hash, avatar_path, contacts) VALUES
+  ('2017-09-20',
+   'ignat.v@gmail.com',
+   'Игнат',
+   '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka',
+   'img/avatar.jpg',
+   '+79787777712');
 
-INSERT INTO users
-SET
-  reg_date      = '2017-09-20',
-  email         = 'kitty_93@li.ru',
-  name          = 'Леночка',
-  password_hash = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa',
-  avatar_path   = 'img/avatar.jpg',
-  contacts      = '+79788888712';
+INSERT INTO users (reg_date, email, name, password_hash, avatar_path, contacts) VALUES
+  ('2017-09-20',
+   'kitty_93@li.ru',
+   'Леночка',
+   '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa',
+   'img/avatar.jpg',
+   '+79788888712');
 
-INSERT INTO users
-SET
-  reg_date      = '2017-09-20',
-  email         = 'warrior07@mail.ru',
-  name          = 'Руслан',
-  password_hash = '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW',
-  avatar_path   = 'img/avatar.jpg',
-  contacts      = 'пишите на мыло warrior07@mail.ru или ищите ВК Руся';
+INSERT INTO users (reg_date, email, name, password_hash, avatar_path, contacts) VALUES
+  ('2017-09-20',
+   'warrior07@mail.ru',
+   'Руслан',
+   '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW',
+   'img/avatar.jpg',
+   'пишите на мыло warrior07@mail.ru или ищите ВК Руся');
 
-INSERT INTO lots
-SET
-  title       = '2014 Rossignol District Snowboard',
-  description = 'Описание отсутствует.',
-  create_date = '2017-09-20 20:04:00',
-  finish_date = '2017-09-22 12:00:00',
-  image       = 'img/lot-1.jpg',
-  start_price = '10999',
-  bet_step    = '100',
-  fav_count   = '0',
-  category_id = '1',
-  author_id   = '1',
-  winner_id   = NULL;
+INSERT INTO lots (title, description, create_date, finish_date, image, start_price, bet_step, fav_count, category_id, author_id, winner_id)
+VALUES ('2014 Rossignol District Snowboard',
+  'Описание отсутствует.',
+  '2017-09-20 20:04:00',
+  '2017-09-22 12:00:00',
+  'img/lot-1.jpg',
+  '10999',
+  '100',
+  '0',
+  '1',
+  '1',
+  NULL);
 
-INSERT INTO lots
-SET
-  title       = 'DC Ply Mens 2016/2017 Snowboard',
-  description = 'Описание отсутствует.',
-  create_date = '2017-09-20 20:04:00',
-  finish_date = '2017-09-22 12:00:00',
-  image       = 'img/lot-2.jpg',
-  start_price = '159999',
-  bet_step    = '100',
-  fav_count   = '5',
-  category_id = '1',
-  author_id   = '2',
-  winner_id   = NULL;
+INSERT INTO lots (title, description, create_date, finish_date, image, start_price, bet_step, fav_count, category_id, author_id, winner_id)
+VALUES ('DC Ply Mens 2016/2017 Snowboard',
+  'Описание отсутствует.',
+  '2017-09-20 20:04:00',
+  '2017-09-22 12:00:00',
+  'img/lot-2.jpg',
+  '159999',
+  '100',
+  '5',
+  '1',
+  '2',
+  NULL);
 
-INSERT INTO lots
-SET
-  title       = 'Крепления Union Contact Pro 2015 года размер L/XL',
-  description = 'Описание отсутствует.',
-  create_date = '2017-09-19 20:04:00',
-  finish_date = '2017-09-20 12:00:00',
-  image       = 'img/lot-3.jpg',
-  start_price = '8000',
-  bet_step    = '100',
-  fav_count   = '0',
-  category_id = '2',
-  author_id   = '1',
-  winner_id   = '3';
+INSERT INTO lots (title, description, create_date, finish_date, image, start_price, bet_step, fav_count, category_id, author_id, winner_id)
+VALUES ('Крепления Union Contact Pro 2015 года размер L/XL',
+  'Описание отсутствует.', '2017-09-19 20:04:00',
+  '2017-09-20 12:00:00', 'img/lot-3.jpg',
+  '8000',
+  '100',
+  '0',
+  '2',
+  '1',
+  '3');
 
-INSERT INTO lots
-SET
-  title       = 'Ботинки для сноуборда DC Mutiny Charocal',
-  description = 'Описание отсутствует.',
-  create_date = '2017-09-16 18:04:00',
-  finish_date = '2017-09-25 19:00:00',
-  image       = 'img/lot-4.jpg',
-  start_price = '10999',
-  bet_step    = '100',
-  fav_count   = '3',
-  category_id = '3',
-  author_id   = '1',
-  winner_id   = NULL;
+INSERT INTO lots (title, description, create_date, finish_date, image, start_price, bet_step, fav_count, category_id, author_id, winner_id)
+VALUES
+  ('Ботинки для сноуборда DC Mutiny Charocal',
+    'Описание отсутствует.',
+    '2017-09-16 18:04:00',
+    '2017-09-25 19:00:00',
+    'img/lot-4.jpg',
+    '10999',
+    '100',
+    '3',
+    '3',
+    '1',
+    NULL);
 
-INSERT INTO lots
-SET
-  title       = 'Куртка для сноуборда DC Mutiny Charocal',
-  description = 'Описание отсутствует.',
-  create_date = '2017-09-20 20:04:00',
-  finish_date = '2017-09-22 12:00:00',
-  image       = 'img/lot-5.jpg',
-  start_price = '7500',
-  bet_step    = '300',
-  fav_count   = '0',
-  category_id = '4',
-  author_id   = '3',
-  winner_id   = NULL;
+INSERT INTO lots (title, description, create_date, finish_date, image, start_price, bet_step, fav_count, category_id, author_id, winner_id)
+VALUES
+  ('Куртка для сноуборда DC Mutiny Charocal',
+    'Описание отсутствует.',
+    '2017-09-20 18:04:00',
+    '2017-09-22 19:00:00',
+    'img/lot-5.jpg',
+    '7500',
+    '300',
+    '0',
+    '4',
+    '3',
+    NULL);
 
-INSERT INTO lots
-SET
-  title       = 'Маска Oakley Canopy',
-  description = 'Описание отсутствует.',
-  create_date = '2017-09-01 20:04:00',
-  finish_date = '2018-03-16 12:00:00',
-  image       = 'img/lot-6.jpg',
-  start_price = '5400',
-  bet_step    = '100',
-  fav_count   = '0',
-  category_id = '6',
-  author_id   = '1',
-  winner_id   = NULL;
+INSERT INTO lots (title, description, create_date, finish_date, image, start_price, bet_step, fav_count, category_id, author_id, winner_id)
+VALUES ('Маска Oakley Canopy',
+  'Описание отсутствует.',
+  '2017-09-20 18:04:00',
+  '2018-03-16 19:00:00',
+  'img/lot-6.jpg',
+  '5400',
+  '100',
+  '0',
+  '6',
+  '1',
+  NULL);
 
-INSERT INTO bets
-SET
-  bet_date  = '2017-09-20 21:00:00',
-  bet_value = '7200',
-  author_id = '2',
-  lot_id    = '6';
+INSERT INTO bets (bet_date, bet_value, author_id, lot_id)
+VALUES ('2017-09-20 21:00:00',
+        '7200',
+        '2',
+        '6'
+);
 
-INSERT INTO bets
-SET
-  bet_date  = '2017-09-20 22:30:00',
-  bet_value = '7500',
-  author_id = '3',
-  lot_id    = '6';
+INSERT INTO bets (bet_date, bet_value, author_id, lot_id)
+VALUES ('2017-09-20 22:30:00',
+        '7500',
+        '3',
+        '6'
+);
 
-INSERT INTO bets
-SET
-  bet_date  = '2017-09-20 09:13:30',
-  bet_value = '9000',
-  author_id = '3',
-  lot_id    = '3';
+INSERT INTO bets (bet_date, bet_value, author_id, lot_id)
+VALUES ('2017-09-20 09:13:30',
+        '9000',
+        '3',
+        '3'
+);
 
 /* Получение категорий */
 SELECT name
